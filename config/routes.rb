cd05066df594c9cc97ce1a3f39ecd6acc52101ec
raditotev <raditotev@gmail.com>
2015-11-06 12:53:49 +0000

    Added index to Events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create]
-  resources :events, only: [:new, :create, :show]
+  resources :events, only: [:index, :new, :create, :show]
 end
